2ff2042d1b16a41dd103c506b70f81532b8ba9f0
feixiao <feixiao2020@sina.com>
2021-06-01 13:47:15 +0800

    解读代码

--- 源码/第15章/SunnyWeather/app/src/main/java/com/sunnyweather/android/logic/Repository.kt
+++ 源码/第15章/SunnyWeather/app/src/main/java/com/sunnyweather/android/logic/Repository.kt
@@ -10,8 +10,10 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlin.coroutines.CoroutineContext
 
+// 新建一个Repository单例类，作为仓库层的统一封装入口
 object Repository {
 
+    // 一般在仓库层中定义的方法，为了能将异步获取的数据以响应式编程的方式通知给上一层，通常会返回一个LiveData对象
     fun searchPlaces(query: String) = fire(Dispatchers.IO) {
         val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
         if (placeResponse.status == "ok") {
@@ -52,6 +54,9 @@ object Repository {
 
     fun isPlaceSaved() = PlaceDao.isPlaceSaved()
 
+    // 这是一个按照liveData()函数的参数接收标准定义的一个高阶函数。
+    // 在fire()函数的内部会先调用一下liveData()函数，然后在liveData()函数的代码块中统一进行了try catch处理，
+    // 并在try语句中调用传入的Lambda表达式中的代码，最终获取Lambda表达式的执行结果并调用emit()方法发射出去。
     private fun <T> fire(context: CoroutineContext, block: suspend () -> Result<T>) =
         liveData<Result<T>>(context) {
             val result = try {
--- 源码/第15章/SunnyWeather/app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
+++ 源码/第15章/SunnyWeather/app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
@@ -20,8 +20,12 @@ object SunnyWeatherNetwork {
 
     private val placeService = ServiceCreator.create(PlaceService::class.java)
 
+    // 当外部调用SunnyWeatherNetwork的searchPlaces()函数时，Retrofit就会立即发起网络请求，同时当前的协程也会被阻塞住。
+    // 直到服务器响应我们的请求之后，await()函数会将解析出来的数据模型对象取出并返回，同时恢复当前协程的执行，
+    // searchPlaces()函数在得到await()函数的返回值后会将该数据再返回到上一层。
     suspend fun searchPlaces(query: String) = placeService.searchPlaces(query).await()
 
+    // ???? 
     private suspend fun <T> Call<T>.await(): T {
         return suspendCoroutine { continuation ->
             enqueue(object : Callback<T> {
